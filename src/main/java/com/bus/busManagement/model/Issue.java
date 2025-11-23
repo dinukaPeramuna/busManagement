8029326b8de691e92cb33fb2e6146dee4432e7c8
dinukaPeramuna <dinukac7pro@gmail.com>
2025-11-23 13:25:28 +0530

    Update Issue.java

--- src/main/java/com/bus/busManagement/model/Issue.java
+++ src/main/java/com/bus/busManagement/model/Issue.java
@@ -1,5 +1,19 @@
-package com.bus.busManagement.model;
 
+package com.bus.system.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+
+@Data
+@Document(collection = "issues")
 public class Issue {
-    
+    @Id
+    private String id;
+    private String type; // "VEHICLE" or "ROUTE"
+    private String description;
+    private String reportedByUserId;
+    private LocalDateTime reportedAt;
 }
+
