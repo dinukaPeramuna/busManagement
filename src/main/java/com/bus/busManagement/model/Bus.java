aa701f6cdf7cffdcfeaff2a70e01db5450c12988
dinukaPeramuna <dinukac7pro@gmail.com>
2025-11-23 13:22:01 +0530

    Update Bus.java

--- src/main/java/com/bus/busManagement/model/Bus.java
+++ src/main/java/com/bus/busManagement/model/Bus.java
@@ -1,5 +1,17 @@
-package com.bus.busManagement.model;
 
+package com.bus.system.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "buses")
 public class Bus {
-    
+    @Id
+    private String id;
+    private String registrationNumber;
+    private String driverId;    // Assigned Driver
+    private String conductorId; // Assigned Conductor
+    private String routeName;   // Default route
 }
